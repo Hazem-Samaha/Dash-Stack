d024bc96c51f7640b64cabe70cad595a93e5a5ee
Hazem <test@example.com>
2026-02-18 03:28:20 +0300

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import AddProduct from './pages/AddProduct.jsx'
 import EditProduct from './pages/EditProduct.jsx'
 const routes=createBrowserRouter([
   {
-    path:"/",
+    path:"/Dash-Stack",
     element:<Auth/>,
     children:[
       {
@@ -22,7 +22,7 @@ const routes=createBrowserRouter([
         element:<Register />
       }]
   },{
-    path:"/dashboard",
+    path:"/Dash-Stack/dashboard",
     element:<Dashboard />,
     children:[
       {
